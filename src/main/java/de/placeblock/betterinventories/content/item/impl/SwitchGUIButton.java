f40740946b8109b19341d50caed72ac1cdde280c
PlaceblockHD <felix.weglehner@gmx.de>
2023-02-05 11:42:49 +0100

    Fixed SwitchGUI nullpointer

--- src/main/java/de/placeblock/betterinventories/content/item/impl/SwitchGUIButton.java
+++ src/main/java/de/placeblock/betterinventories/content/item/impl/SwitchGUIButton.java
@@ -2,6 +2,7 @@ package de.placeblock.betterinventories.content.item.impl;
 
 import de.placeblock.betterinventories.content.item.GUIButton;
 import de.placeblock.betterinventories.gui.GUI;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -13,12 +14,14 @@ public class SwitchGUIButton extends GUIButton {
     private final Supplier<GUI> targetGUI;
 
     public SwitchGUIButton(GUI gui, ItemStack item, Supplier<GUI> targetGUI) {
-        super(gui, item);
+        super(gui, item, targetGUI == null ? null : Sound.UI_BUTTON_CLICK);
         this.targetGUI = targetGUI;
     }
 
     @Override
     public void onClick(Player player, int slot) {
-        this.targetGUI.get().showPlayer(player);
+        if (this.targetGUI != null) {
+            this.targetGUI.get().showPlayer(player);
+        }
     }
 }
