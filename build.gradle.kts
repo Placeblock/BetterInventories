b661e87252e5bda65560ff8c475644c2d29836b3
felix <felix.weglehner@gmx.de>
2023-08-10 22:44:10 +0200

    Tried to include JavaDoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 }
 
 java {
+    withJavadocJar()
+    withSourcesJar()
     // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
