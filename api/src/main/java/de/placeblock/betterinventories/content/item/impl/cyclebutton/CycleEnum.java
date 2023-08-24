1a3453a6d458897eb6a485a6f5151d86f1363d31
felix <felix.weglehner@gmx.de>
2023-08-25 01:55:10 +0200

    Started

--- api/src/main/java/de/placeblock/betterinventories/content/item/impl/cyclebutton/CycleEnum.java
+++ api/src/main/java/de/placeblock/betterinventories/content/item/impl/cyclebutton/CycleEnum.java
@@ -5,12 +5,28 @@ import org.bukkit.Material;
 
 import java.util.List;
 
+/**
+ * Interface used by the {@link CycleGUIButton}
+ * Has to be implemented by the Enum to provide needed data to {@link CycleGUIButton}
+ */
 public interface CycleEnum {
 
+    /**
+     * The Material of the ItemStack for the Enum-value
+     * @return The Material
+     */
     Material getMaterial();
 
+    /**
+     * The title of the ItemStack for the Enum-value
+     * @return The title
+     */
     TextComponent getTitle();
 
+    /**
+     * The lore of the ItemStack for the Enum-value
+     * @return The lore
+     */
     List<TextComponent> getLore();
 
 }
