7c0b5af657b18d3ef9169f44e6e50100999f0c26
felix <felix.weglehner@gmx.de>
2023-09-10 01:29:31 +0200

    Fixed paginator width

--- api/src/test/java/de/placeblock/betterinventories/PaginatorGUIPaneTest.java
+++ api/src/test/java/de/placeblock/betterinventories/PaginatorGUIPaneTest.java
@@ -5,6 +5,7 @@ import be.seeseemelk.mockbukkit.MockPlugin;
 import de.placeblock.betterinventories.builder.content.GUIItemBuilder;
 import de.placeblock.betterinventories.builder.content.PaginatorBuilder;
 import de.placeblock.betterinventories.builder.gui.ChestGUIBuilder;
+import de.placeblock.betterinventories.content.item.GUIItem;
 import de.placeblock.betterinventories.content.pane.impl.paginator.PaginatorControlsPosition;
 import de.placeblock.betterinventories.content.pane.impl.paginator.PaginatorGUIPane;
 import de.placeblock.betterinventories.gui.impl.ChestGUI;
@@ -13,6 +14,8 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 
 public class PaginatorGUIPaneTest {
     private MockPlugin plugin;
@@ -39,6 +42,22 @@ public class PaginatorGUIPaneTest {
         chestGUI.getCanvas().setSection(paginator);
         chestGUI.update();
         System.out.println(chestGUI.renderContent());
+    }
+
+    @Test
+    public void testPaginatorWithControls() {
+        ChestGUI gui = new ChestGUIBuilder(this.plugin)
+                .minHeight(1).maxHeight(6)
+                .title(Component.empty())
+                .build();
+        PaginatorGUIPane paginator = new PaginatorBuilder(gui)
+                .adoptMinMax(gui.getCanvas())
+                .defaultControls(PaginatorControlsPosition.SPACE_EVENLY)
+                .build();
+        paginator.addItems(Collections.nCopies(20, new GUIItem(gui, new ItemStack(Material.DIAMOND_BLOCK))));
+        gui.getCanvas().setSection(paginator);
+        gui.update();
+        System.out.println(gui.renderContent());
     }
 
 }
